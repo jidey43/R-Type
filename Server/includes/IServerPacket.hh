4e96707cd52e328241a71b942b695b06eaa86859
Yasker <arthur.ngovan@gmail.com>
2015-12-07 19:01:09 +0100

    merged

---
+++ Server/includes/IServerPacket.hh
@@ -0,0 +1,36 @@
+#ifndef ISERVERPACKET_H_
+# define ISERVERPACKET_H_
+
+#include "NetworkDefines.h"
+
+typedef struct
+{
+  int				data;
+} __attribute__ ((packed))	integerData;
+
+typedef struct
+{
+  int				id;
+  int				port;
+} __attribute__ ((packed))	GameInfoData;
+
+typedef struct
+{
+  int				id;
+  char				gameName[256];
+  char				players[256];
+} __attribute__ ((packed))	DesGameData;
+
+typedef integerData GameOverData;
+
+
+class IServerPacket
+{
+public:
+  virtual ~IServerPacket() {};
+
+public:
+  virtual std::string const&	deserialize() = 0;
+};
+
+#endif
