b28c40ed1105c7dfbd578268a36316685786e7f2
roman <roman.grout@epitech.eu>
2015-12-08 11:47:16 +0100

    fix bug dans le main

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -7,11 +7,16 @@ AssetsController *ac;
 
 int main()
 {
-  ac = new AssetsController("../../assets/");
-  if (ac->loadAssets() == false)
-    {
-      std::cerr << "assets could not be loaded, aborting . . ." << std::endl;
-      return -1;
-    }
-  return 0;
+	ac = new AssetsController("../../assets/");
+	if (ac->loadAssets() == false)
+	{
+		std::cerr << "assets could not be loaded, aborting . . ." << std::endl;
+		return -1;
+	}
+
+	vc = new ViewController;
+
+	Manager m;
+
+	m.loop();
 }
