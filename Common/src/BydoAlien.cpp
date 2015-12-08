e54b848bf35449b79fa244dbba53c4146791ed87
Giudici <aurelien.giudici@epitech.eu>
2015-12-08 15:30:32 +0100

    ending merging, it was long

--- Common/src/BydoAlien.cpp
+++ Common/src/BydoAlien.cpp
@@ -12,16 +12,10 @@ void		BydoAlien::update()
 {}
 
 IObject		*BydoAlien::BasicShoot()
-{ 
-<<<<<<< HEAD
+{
   sf::Vector2i pos;
 
   pos.x = _pos.x + _speed.x;
   pos.y = _pos.y;
   return new BasicAlienProjectile(_speed, pos, 1.0);
-=======
-	//debug
-	
-	return NULL;
->>>>>>> 802ffea06c62fe3665e96ee80064f0495108c129
 }
--- Common/src/Player.cpp
+++ Common/src/Player.cpp
@@ -14,16 +14,10 @@ void		Player::update()
 
 IObject		*Player::BasicShoot()
 {
-<<<<<<< HEAD
   sf::Vector2i pos;
   
   pos.x = _pos.x + _size.x;
   pos.y = _pos.y;
   return new BasicPlayerProjectile(_speed, pos, 1.0);
-=======
-  //sf::Vector2i	pos(_size.x + _pos.x, _pos.y);
-  //IObject *shoot = new BasicPlayerProjectile(_pos);
-	return NULL;
-  //return (shoot);
->>>>>>> 802ffea06c62fe3665e96ee80064f0495108c129
+
 }
