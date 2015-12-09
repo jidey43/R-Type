5ae017fd3690abee8aec0022cdfa99ae746b0b69
Simon Vadée <simon.vadee@epitech.eu>
2015-12-09 16:33:58 +0100

    oubli de labstraction

---
+++ Server/includes/AServerPacket.hh
@@ -0,0 +1,23 @@
+#ifndef ASERVERPACKET_H_
+# define ASERVERPACKET_H_
+
+# include "IServerPacket.hh"
+
+template <typename T>
+class AServerPacket : public IServerPacket<T>
+{
+public:
+  AServerPacket(T);
+  virtual ~AServerPacket();
+
+protected:
+ T			_command;
+
+public:
+  T			getCommandType() const;
+  virtual std::string const& deserialize() = 0;
+};
+
+# include "AServerPacket.tpp"
+
+#endif /* !ASERVERPACKET_H_ */
