d574d8d436a4e88c240a9e39289581fc27899bb7
Simon Vadée <simon.vadee@epitech.eu>
2015-12-12 14:55:21 +0100

    forgot one lol

---
+++ Commoni/includes/CAuthUDPPacket.h
@@ -0,0 +1,23 @@
+#ifndef CAUTCHUDPPACKET_H_
+# define CAUTCHUDP PACKET_H_
+
+# include "AClientPacket.hh"
+
+class CAuthUDPPacket : public AClientPacket<ClientUDPCommand>
+{
+ public:
+  CAuthUDPPacket(ClientUDPCommand, int, int, std::string const&);
+  CAuthUDPPacket(ClientUDPHeader*);
+  ~CAuthUDPPacket();
+
+  std::string const&		deserialize();
+  bool				checkHeader();
+  void				setRawData(std::string const&);
+  CAuthUDPData*			getData() const;
+
+ private:
+  CAuthUDPData*			_data;
+  ClientUDPHeader*		_header;
+};
+
+#endif
