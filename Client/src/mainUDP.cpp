4adbf413560089055a124913d1b011aeae347e4d
Simon Vadée <simon.vadee@epitech.eu>
2015-12-13 15:23:59 +0100

    boucle du main test client udp

--- Client/src/mainUDP.cpp
+++ Client/src/mainUDP.cpp
@@ -10,29 +10,26 @@ AssetsController *ac;
 
 int main(int ac, char **av)
 {
-  CUDPNetworkHandler *nh = new CUDPNetworkHandler("127.0.0.1", av[1]);
-  std::string		str;
-  std::string		str2;
+  CUDPNetworkHandler			*nh = new CUDPNetworkHandler("127.0.0.1", av[1]);
+  std::string				str;
+  IServerPacket<ServerUDPResponse>	*packet;
+
 
   nh->initSocket();
   while (1)
     {
       std::getline(std::cin, str);
-      str2 = str.substr(str.find(" "), std::string::npos);
       int i = std::atoi(str.c_str());
       switch (i)
       	{
 	case 1:
-	  _network->sendToServer(new CAuthUDPPacket(CAUTH_UDP, 0, "toto"));
-	  _network->receiveFromServer();
+	  _network->send(new CAuthUDPPacket(CAUTH_UDP, 0, "toto"));
 	  break;
 	case 2:
-	  _network->sendToServer(new FirePacket(AUTH_TCP, 10, 10));
-	  _network->receiveFromServer();
+	  _network->send(new FirePacket(AUTH_TCP, 10, 10));
 	  break;
-	// case 2:
-	//   _network->sendToServer()
       	}
-      // std::cout << "SERVER : " << _network->getPacket() << std::endl;
+      if (nh->selectServer())
+	packet = nh->receive()
     }
 }
