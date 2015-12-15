d7c9eb16ce5a9258fd3b1b4b44a002f4e8ce6bc3
Simon Vadée <simon.vadee@epitech.eu>
2015-12-15 13:57:11 +0100

    debug ok !

--- Commoni/src/CrePlayPacket.cpp
+++ Commoni/src/CrePlayPacket.cpp
@@ -9,7 +9,7 @@ CrePlayPacket::CrePlayPacket(ServerUDPResponse resp, int idx, int id, float x, f
   _header->idx = idx;
   _data->id = id;
   _data->x = x;
-  _data->x = y;
+  _data->y = y;
   _data->magic = MAGIC;
 }
 
