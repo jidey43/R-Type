6f5addf8536722d20b2cdfb1f368978d8eb6ccc0
roman <roman.grout@epitech.eu>
2015-12-21 20:31:20 +0100

    tir moins buggé .. un peu :)

--- Client/src/Explosion.cpp
+++ Client/src/Explosion.cpp
@@ -5,6 +5,9 @@ Explosion::Explosion(sf::Vector2f pos) : GraphicalItem(), _pos(pos)
     _sprite->setTexture(*(ac->getExplosion()));
     _sprite->setPosition(sf::Vector2f(_pos.x - (EXPLOSION_X / 2), _pos.y - (EXPLOSION_Y / 2)));
     _sprite->setTextureRect(sf::IntRect(0, 0, 120, 100));
+    _referential = sf::milliseconds(100);
+    _stage = 1;
+    _lastUpdate = sf::seconds(0);
 }
 
 Explosion::~Explosion()
@@ -14,5 +17,14 @@ Explosion::~Explosion()
 
 bool                Explosion::update(sf::Clock const& clock)
 {
-    
+    if ((clock.getElapsedTime() - _lastUpdate) > _referential)
+    {
+    _lastUpdate = clock.getElapsedTime();
+    _sprite->setTextureRect(sf::IntRect(0, 0, 120 * _stage, 100));
+   // _sprite->setPosition(sf::Vector2f(_pos.x - (120 *_stage) , _pos.y));
+    _stage += 1;
+    if (_stage == 5)
+        return false;
+    }
+   return true;
 }
