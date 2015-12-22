6d55b4c2d48cf8dd9c014bebeb01c390003813eb
YaskOne <arthur.ngovan@gmail.com>
2015-12-22 11:26:05 +0100

    major merge

--- Common/src/Alien.cpp
+++ Common/src/Alien.cpp
@@ -13,7 +13,7 @@ const float		&Alien::getCoeff() const
   return _coeff;
 }
 
-bool		Alien::update(std::vector<IObject*>& map, sf::Clock const& clock)
+bool		Alien::update(sf::Clock const& clock, std::vector<IObject*>& map)
 {
 }
 
