f53ea166145f634714f264768e03f6a199833384
Simon Vadée <simon.vadee@epitech.eu>
2015-12-23 23:40:55 +0100

    patch bad header request ?

--- Client/src/CUDPNetworkHandler.cpp
+++ Client/src/CUDPNetworkHandler.cpp
@@ -37,7 +37,7 @@ IServerPacket<ServerUDPResponse>*	CUDPNetworkHandler::receive()
     {
       packet = _factory->build(_header);
       if (!packet || !packet->checkHeader())
-		throw Exceptions::BadHeaderRequest("Error, received bad Header from serv");
+	return NULL;
       buff = new char[_header->size];
       memset(buff, 0, _header->size);
       try
