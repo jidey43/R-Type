3af33205c1324c25ac21e572af85a4430d7cefba
girard_r <girard_r@epitech.eu>
2015-12-27 19:56:04 +0100

    push pattern bonus speed

--- Common/src/BonusSpeed.cpp
+++ Common/src/BonusSpeed.cpp
@@ -4,7 +4,9 @@ BonusSpeed::BonusSpeed(sf::Vector2f const& speed,
 		       sf::Vector2f const& pos,
 		       unsigned int id,
 		       float coeff)
-  : ABonus(speed, pos, sf::Vector2i(50, 50), id)
+  : ABonus(speed, pos, sf::Vector2i(50, 50), id),
+    _stay(false),
+    _fact(-1)
 {
 }
 
@@ -22,7 +24,15 @@ bool		BonusSpeed::update(const sf::Clock& clock)
   if (_pos.x + _size.x < 0 || _pos.x > MAP_SIZE_X
       || _pos.y + _size.y < 0 || _pos.y > MAP_SIZE_Y)
     _isAlive = false;
-  this->_pos.x = this->_pos.x - this->_speed.x;
+  if (this->_pos.x <= MAP_SIZE_X / 3)
+    {
+      _fact *= -1;
+      _stay = true;
+    }
+  if (_stay && this->_pos.x >= 2 * MAP_SIZE_X / 3)
+    _fact *= -1;
+  this->_pos.x = this->_pos.x + _fact * this->_speed.x;
+  this->_pos.y -= 1;
   return true;
 }
 
